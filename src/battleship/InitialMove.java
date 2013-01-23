d3d5c6c3a8777c64131f2e3f0ce0eec66f75bea3
Eric <congpeng89@gmail.com>
2013-01-23 12:59:23 -0600

    Added interface for ships.
    This will prompt the user to put in the coordinates for the placement of
    the ships.

---
+++ src/battleship/InitialMove.java
@@ -0,0 +1,10 @@
+package battleship;
+
+
+public interface InitialMove {
+    public void addAirCraft5(int x,int y);
+    public void addBattleship4(int x,int y);
+    public void addSubmarine3(int x,int y);
+    public void addDestroyer3(int x,int y);
+    public void addPatrolBoat2(int x,int y);
+}
