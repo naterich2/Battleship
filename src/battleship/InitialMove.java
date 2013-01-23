918deaa061dfb7de7e27e7aba5b3667aa6a49f7a
Eric <congpeng89@gmail.com>
2013-01-23 13:19:22 -0600

    Change coordinates to number and char.
    Instead of doing a number and a number, doing a number and a letter
    would be clearer for the user to input with a number and a letter.

--- src/battleship/InitialMove.java
+++ src/battleship/InitialMove.java
@@ -2,9 +2,9 @@ package battleship;
 
 
 public interface InitialMove {
-    public void addAirCraft5(int x,int y);
-    public void addBattleship4(int x,int y);
-    public void addSubmarine3(int x,int y);
-    public void addDestroyer3(int x,int y);
-    public void addPatrolBoat2(int x,int y);
+    public void addAirCraft5(int x,char y);
+    public void addBattleship4(int x,char y);
+    public void addSubmarine3(int x,char y);
+    public void addDestroyer3(int x,char y);
+    public void addPatrolBoat2(int x,char y);
 }
